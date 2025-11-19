a8b4098e8a3c40a3ffb84d3cc580b44eb98eb788
Ajang-Deng98 <a.deng3@alustudent.com>
2025-11-20 00:24:21 +0200

    Remove duplicate bastion resource from main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -35,3 +35,16 @@ data "aws_ami" "ubuntu" {
     values = ["available"]
   }
 }
+
+# EC2 Instances with fixed AMI
+resource "aws_instance" "bastion" {
+  ami           = "ami-000c63f74be5cb1c7"  # fixed AMI
+  instance_type = "t2.micro"
+  key_name      = var.key_pair_name
+  subnet_id     = aws_subnet.public.id
+  vpc_security_group_ids = [aws_security_group.app_sg.id]
+
+  tags = { Name = "bastion-host" }
+}
+
+
