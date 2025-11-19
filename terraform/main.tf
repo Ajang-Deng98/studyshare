10dd8bcb4ad9ea7830e865ffaadb88c1ffa34df7
Ajang-Deng98 <a.deng3@alustudent.com>
2025-11-20 00:04:35 +0200

    Fix Ubuntu AMI data source filters

--- terraform/main.tf
+++ terraform/main.tf
@@ -24,4 +24,14 @@ data "aws_ami" "ubuntu" {
     name   = "name"
     values = ["ubuntu/images/hvm-ssd/ubuntu-22.04-amd64-server-*"]
   }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  filter {
+    name   = "state"
+    values = ["available"]
+  }
 }
