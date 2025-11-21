e66eb07512af697cf5843471a3aabd26f2767416
Ajang-Deng98 <a.deng3@alustudent.com>
2025-11-21 18:30:05 +0200

    Fix Jest configuration for ES modules

---
+++ frontend/jest.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
+  moduleNameMapper: {
+    '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+  },
+  transformIgnorePatterns: [
+    'node_modules/(?!(axios)/)'
+  ],
+  collectCoverageFrom: [
+    'src/**/*.{js,jsx,ts,tsx}',
+    '!src/**/*.d.ts',
+    '!src/index.tsx',
+    '!src/reportWebVitals.ts',
+  ],
+};
