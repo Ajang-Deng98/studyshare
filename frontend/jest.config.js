a529668c09bb75e491eb2d7cc8d9338d040b2b12
Ajang-Deng98 <a.deng3@alustudent.com>
2025-11-21 18:37:32 +0200

    Fix Jest configuration for axios ES modules

--- frontend/jest.config.js
+++ frontend/jest.config.js
@@ -3,10 +3,20 @@ module.exports = {
   setupFilesAfterEnv: ['<rootDir>/src/setupTests.ts'],
   moduleNameMapper: {
     '\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^axios$': require.resolve('axios'),
   },
   transformIgnorePatterns: [
-    'node_modules/(?!(axios)/)'
+    'node_modules/(?!(axios)/)',
   ],
+  transform: {
+    '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', {
+      presets: [
+        ['@babel/preset-env', { targets: { node: 'current' } }],
+        ['@babel/preset-react', { runtime: 'automatic' }],
+        '@babel/preset-typescript'
+      ]
+    }]
+  },
   collectCoverageFrom: [
     'src/**/*.{js,jsx,ts,tsx}',
     '!src/**/*.d.ts',
